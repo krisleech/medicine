dcac6c50b155f10c99adf5a43f28da11ae97916a
Kris Leech <kris.leech@gmail.com>
2015-02-13 23:24:18 +0000

    extract methods

--- lib/medicine/define_methods.rb
+++ lib/medicine/define_methods.rb
@@ -14,26 +14,33 @@ module Medicine
 
     def call
       assert_all_dependencies_met
-      object.instance_variable_set("@injections", injections)
-      dependencies.each do |dependency|
-        object.define_singleton_method dependency.method_name do
-          @injections.fetch(dependency.name) { dependency.default }
-        end
-        object.singleton_class.class_eval { private dependency.method_name }
-      end
+      set_injections_var
+      dependencies.each { |dependency| build_method(dependency) }
     end
 
     private
 
     attr_reader :object, :dependencies, :injections
 
+    def build_method(dependency)
+      object.define_singleton_method dependency.method_name do
+        @injections.fetch(dependency.name) { dependency.default }
+      end
+      object.singleton_class.class_eval { private dependency.method_name }
+    end
+
+    # set ivar which is later accessed by the defined methods
+    def set_injections_var
+      object.instance_variable_set("@injections", injections)
+    end
+
     def assert_all_dependencies_met
       raise RequiredDependencyError, "pass all required dependencies (#{unmet_dependencies.join(', ')}) in to initialize" unless unmet_dependencies.empty?
     end
 
     def unmet_dependencies
-      dependencies.without_default.select do |dependency|
-        !injections.include?(dependency.name)
+      dependencies.without_default.reject do |dependency|
+        injections.include?(dependency.name)
       end
     end
 
